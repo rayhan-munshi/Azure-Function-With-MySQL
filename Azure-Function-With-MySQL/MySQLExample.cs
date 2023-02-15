3402d8c36f29118194eea88b3a9ac08557f8ba86
Rayhan <rayhan.tonmoy@gmail.com>
2023-02-15 14:29:45 +0600

    example code added

--- Azure-Function-With-MySQL/MySQLExample.cs
+++ Azure-Function-With-MySQL/MySQLExample.cs
@@ -7,29 +7,122 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using MySqlConnector;
+using System.Collections.Generic;
 
 namespace Azure_Function_With_MySQL
 {
+    public class Person
+    {
+        public int Personid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
     public static class MySQLExample
     {
-        [FunctionName("GetMySQLData")]
-        public static async Task<IActionResult> Run(
+        static readonly MySqlConnection connection = new(Environment.GetEnvironmentVariable("MySQLConnectionStr"));
+        [FunctionName("create-table")]
+        public static async Task<IActionResult> CreateTable(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            //var connStringBuilder = new MySqlConnectionStringBuilder
+            //{
+            //    Server = "tonmoydb.mysql.database.azure.com",
+            //    Database = "mytestdb",
+            //    UserID = "tonmoyadmin",
+            //    Password = "ixora@123",
+            //    SslMode = MySqlSslMode.Required,
+            //    SslCa= "DigiCertGlobalRootCA.crt.pem"
+            //};
+
+            await connection.OpenAsync();
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS Persons (
+                                    Personid int NOT NULL AUTO_INCREMENT,
+                                    LastName varchar(255) NOT NULL,
+                                    FirstName varchar(255),
+                                    Age int,
+                                    PRIMARY KEY (Personid)
+                                );";
+            await cmd.ExecuteNonQueryAsync();
+            await connection.CloseAsync();
+
+            return new OkObjectResult("Persons table created!");
+        }
+
+        [FunctionName("drop-table")]
+        public static async Task<IActionResult> DropTable(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            var str = Environment.GetEnvironmentVariable("MySQLConnectionStr");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            await connection.OpenAsync();
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = @"DROP TABLE Persons;";
+            await cmd.ExecuteNonQueryAsync();
+            await connection.CloseAsync();
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult("Persons table dropped!");
+        }
+
+        [FunctionName("insert-data")]
+        public static async Task<IActionResult> InsertData(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var str = Environment.GetEnvironmentVariable("MySQLConnectionStr");
+
+            await connection.OpenAsync();
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = @"INSERT INTO `mytestdb`.`persons` (`LastName`, `FirstName`, `Age`) VALUES ('Rayhan', 'Tonmoy', '36');";
+            await cmd.ExecuteNonQueryAsync();
+            await connection.CloseAsync();
+
+
+            return new OkObjectResult("Data inserted!");
+        }
+
+        [FunctionName("get-data")]
+        public static async Task<IActionResult> GetData(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var str = Environment.GetEnvironmentVariable("MySQLConnectionStr");
+            var personList = new List<Person>();
+            await connection.OpenAsync();
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = @"SELECT * FROM mytestdb.persons;";
+            using var reader = await cmd.ExecuteReaderAsync();
+            
+
+            while (await reader.ReadAsync())
+            {
+                var person = new Person()
+                {
+                    Personid = reader.GetInt32(0),
+                    FirstName = reader.GetString(1),
+                    LastName = reader.GetString(2),
+                    Age = reader.GetInt32(3),
+                };
+                personList.Add(person);
+            }
+
+            await connection.CloseAsync();
+
+
+            return new OkObjectResult(personList);
         }
     }
 }
